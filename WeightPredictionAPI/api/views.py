817317db8e5a69422eae64a2c80cd4649b5183a6
srpatnaik07 <48448680+srpatnaik07@users.noreply.github.com>
2023-07-06 18:53:27 +0530

    first commit

---
+++ WeightPredictionAPI/api/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+import numpy as np
+import pandas as pd
+from .apps import ApiConfig
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# Create your views here.
+class WeightPrediction(APIView):
+    def post(self,request):
+        data = request.data
+        height = data['Height']
+        gender = data['Gender']
+        if gender == 'Male':
+            gender = 0
+        
+        elif gender =='Female':
+            gender = 1
+
+        else:
+            return Response("Gender field is invalid",status=400)
+        lin_reg_model = ApiConfig.model
+        weight_predicted = lin_reg_model.predict([[gender,height]])[0][0]
+        weight_predicted = np.round(weight_predicted,1)
+        response_dict = {"Predicted Weight (kg)": weight_predicted}
+        return Response(response_dict,status=200)
+    
